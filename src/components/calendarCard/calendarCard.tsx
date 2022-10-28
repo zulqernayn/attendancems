ee4ea73d81fa38171b75ba803c4d4de2ff290dd9
zulqernayn <zulqernayn@gmail.com>
2022-10-28 18:11:36 +0500

    2

--- src/components/calendarCard/calendarCard.tsx
+++ src/components/calendarCard/calendarCard.tsx
@@ -1,21 +1,20 @@
-export default function CalendarCard({dates=[]}:{dates:Array<string>}){
+import { returnMonthFillDates, WEEK_DAYS } from "../../utils/utils"
 
-    const MONTHS=new Set(dates.map(date=>new Date(date.split("T")[0]).getMonth()+1))
-    
+export default function CalendarCard({dates=[],month=0}:{dates:Array<string>,month:number}){
+
     const today=new Date()
+
     
-    const WEEK_DAYS=[
-        "Su",
-        "Mo",
-        "Tu",
-        "We",
-        "Th",
-        "Fr",
-        "Sa",
-    ]
-    
-    const MONTH_DATES=new Array(30).fill(null).map((_,i)=>i+1)
-    const BLANKS=new Array(new Date(today.getFullYear(),today.getMonth(),1).getDay()).fill(null)
+    function calendarDatesMapping(date:number|null|string,i:number){
+        let filterDatesOfThisMonth:Array<string|number|null>=dates.filter(date=>{
+            return new Date(date).getMonth()===month
+        })
+        filterDatesOfThisMonth=filterDatesOfThisMonth.map(date=>new Date(date).getDay())
+        if(filterDatesOfThisMonth.includes(date))
+            <p className="bg-sky-300">{date}</p>
+        else
+            <p>{date}</p>
+    }
     
     return(
         <div className="flex flex-col gap-3 bg-zinc-900 h-fit p-4 rounded-xl">
@@ -25,9 +24,7 @@ export default function CalendarCard({dates=[]}:{dates:Array<string>}){
                 {WEEK_DAYS.map(day=><p>{day}</p>)}
                 </div>
                <div className="grid grid-cols-7 gap-2 justify-items-center">
-                {[...BLANKS,...MONTH_DATES].map(date=>{
-                    const filteredDate=dates.filter(el=>new Date(el).getDay()===date)
-                })}
+                {returnMonthFillDates(new Date()).map(calendarDatesMapping)}
                 </div>
            </div>
         </div>
